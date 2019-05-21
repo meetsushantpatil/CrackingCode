91e76920f9eb9e6f63d04c3e94453e4e797b15be
Sushant Patil <meetsushantpatil@gmail.com>
2019-05-20 22:54:26 -0400

    Adding Selection Sort

---
+++ SelectionSort.py
@@ -0,0 +1,36 @@
+
+##Code to do Bubble Sort###
+
+##Keep left part of the array sorted
+
+global step_counter;
+step_counter = 0;
+
+def selection_sort(list):
+    global step_counter;
+    for j in range(0,len(list)-1):
+        step_counter += 1;
+        index = find_smallest ( list[j:len(list)] ) + j;
+        if (index != j):
+            list[index], list[j] = list[j], list[index];
+    print("The sorted list is :" + str(list))
+    print("Steps needed to sort :" + str(step_counter));
+
+
+def find_smallest(list):
+    global step_counter;
+    smallest_num = list[0];
+    smallest_num_index = 0;
+    for i in range(0, len(list)-1):
+        step_counter += 1;
+        if(smallest_num > list[i+1]):
+            smallest_num = list[i+1];
+            smallest_num_index = (i+1);
+    return smallest_num_index;
+
+
+a = [0, 11, 2, -7, 200];
+#print(find_smallest(a));
+print(selection_sort(a));
+
+
